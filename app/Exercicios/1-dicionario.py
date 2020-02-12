3817ecaf3eb74e2edeb5a7688faac21326b82ccb
Maykon D. Granemann <maykondgranemann@gmail.com>
2020-02-12 19:45:44 -0300

    Aula3 - Ex1

---
+++ app/Exercicios/1-dicionario.py
@@ -0,0 +1,5 @@
+# Criar o cadastro de um funcionário
+# Solicitar o nome através do console e armazenar em uma variável
+# Solicitar o rg através do console e armazenar em uma variável
+# Cria um dicionário com os dados lidos
+# Imprimir o dicionário criado
